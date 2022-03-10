7c22e44c846df07e104a5e84ba4a76f529843fb3
chii <chiharu.5656.s@gmail.com>
2022-03-10 22:35:20 +0900

    initialize repository

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var Black  = '#211f20';
+var White = "white";
+var Gray = "#d1d2d4";
+
+function setup() {
+    createCanvas(480, 480);
+    background(Gray);
+    textFont("Abril Fatface");
+    textSize(100);
+}
+
+function draw() {
+    text("Hello", 100, 100);
+    fill(White);
+}
